f5950bf668f710dca3013afa6346c273d1485c08
Wojciech Napierała <wnapierala@hotmail.com>
2023-07-23 18:56:10 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { getAllQueries, addQuery, editQuery, deleteQuery } from './components/utils';
+import QueryForm from './components/QueryForm';
+import QueryDashboard from './components/QueryDashboard';
 
 function App() {
+  const [queries, setQueries] = useState([]);
+
+  useEffect(() => {
+    fetchQueries();
+  }, []);
+
+  const fetchQueries = () => {
+    const allQueries = getAllQueries();
+    setQueries(allQueries);
+  };
+
+  const handleAddQuery = (newQuery) => {
+    addQuery(newQuery);
+    fetchQueries();
+  };
+
+  const handleEditQuery = (queryId, updatedQuery) => {
+    editQuery(queryId, updatedQuery);
+    fetchQueries();
+  };
+
+  const handleDeleteQuery = (queryId) => {
+    deleteQuery(queryId);
+    fetchQueries();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>Query Manager App</h1>
+      <QueryForm onAddQuery={handleAddQuery} />
+      <QueryDashboard
+        queries={queries}
+        onEditQuery={handleEditQuery}
+        onDeleteQuery={handleDeleteQuery}
+      />
     </div>
   );
 }
